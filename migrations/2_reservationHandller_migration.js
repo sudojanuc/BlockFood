f4b58f189f316b6d9fb390459c08844f2cf3c6bd
Applecakeman <sebastian.baier@live.de>
2021-06-06 18:43:06 +0200

    -update migration file -update LICENCE File

--- migrations/2_reservationHandller_migration.js
+++ migrations/2_reservationHandller_migration.js
@@ -4,13 +4,24 @@ const Unit = artifacts.require("Unit");
 const Reservation = artifacts.require("Reservation");
 
 module.exports = function (deployer) {
-  deployer.deploy(Provider, {overwrite: false}).then(function() {
-    //return deployer.deploy(Unit, Provider.address)
-    return deployer.deploy(Unit, {overwrite: false})
-  }).then(function(){
-    return deployer.deploy(Reservation, Unit.address)
-  }).then(function() {
-    //return deployer.deploy(ReservationHandler, Provider.address, Unit.address, Reservation.address );
-    return deployer.deploy(ReservationHandler, {overwrite: false});
-  });
-}
+  deployer
+    .deploy(Provider)
+    // .deploy(Provider, { overwrite: false })
+    .then(function () {
+      return deployer.deploy(Unit, Provider.address);
+      // return deployer.deploy(Unit, { overwrite: false });
+    })
+    .then(function () {
+      return deployer.deploy(Reservation, Unit.address);
+      // return deployer.deploy(Reservation, { overwrite: false });
+    })
+    .then(function () {
+      return deployer.deploy(
+        ReservationHandler,
+        Provider.address,
+        Unit.address,
+        Reservation.address
+      );
+      // return deployer.deploy(ReservationHandler, { overwrite: false });
+    });
+};
